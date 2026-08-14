eaf5c411861c7e8ddb89120479c20b619f33e414
Yonkode <moulero.gbaguidi@gmail.com>
2026-08-14 03:17:37 +0100

    feat(product): implentation de la logic CRUD dans ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,62 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index(){
+        $products = Product::latest()->get();
+
+        return view('products.index', compact('products'));
+    }
+
+    public function create(){
+        return view('products.create');
+    }
+
+    public function store(Request $request){
+        $validation = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|sring',
+            'price' => 'required|numeric|min:0',
+            'stock' => 'required|integer|min:0',
+        ]);
+
+        Product::create($validated);
+
+        return redirect()->route('product.index')
+                          ->with('success', 'Produit ajoute avec succes.');
+    }
+
+    public function show(Product $product)
+    {
+        return view('products.show', compact('product'));
+    }
+
+    public function edit(Product $product){
+        return view('products.edit', compact('product'));
+    }
+
+    public function update(Request $request, Product $product){
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'required|string',
+            'price' =>'required|numeric|min:0',
+            'stock' => 'required|integer|min:0',
+        ]);
+
+        $product->update($validated);
+
+        return redirect()->route('products.index')
+                         ->with('sucess', 'Produit modifie avec succes.');
+    }
+
+    public function destroy(Product $product){
+        $product->delete();
+
+        return redirect()->route('products.index')
+                         ->with('success', 'Produit supprime avec succes.');
+    }
 }
